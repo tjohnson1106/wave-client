e96f8d750d251474d2fa9645f6f31a6ec69bd4a2
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-16 17:20:20 -0500

    added react native navigation xcode proj file to x-code and configured appDelegate

---
+++ src/screens/FeedsScreen/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+const FeedsScreen = () => (
+  <View>
+    <Text>FeedsScreen</Text>
+  </View>
+);
+
+export default FeedsScreen;
---
+++ src/screens/index.js
@@ -0,0 +1,8 @@
+import { Navigation } from "react-native-navigation";
+
+import FeedsScreen from "./FeedsScreen";
+
+
+export const registerScreens = () => {
+    Navigation.registerComponent("wave_client.FeedsScreen")
+}
