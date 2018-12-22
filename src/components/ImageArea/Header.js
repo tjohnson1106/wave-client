0c273898ec81fc560547228a24d3dd7724944598
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-22 16:13:54 -0500

    created metric avatar container layout

--- src/components/ImageArea/Header.js
+++ src/components/ImageArea/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import { makeCircle } from "../../utils/themes";
+
 export default function Header() {
   return (
     <View style={styles.root}>
-      <View style={styles.userMetaWrapper}>{}</View>
+      <View style={styles.userMetaWrapper}>
+        <View style={styles.avatarWrapper}>
+          <View style={styles.avatarImg} />
+        </View>
+      </View>
+      <View style={styles.buttonWrapper}>{}</View>
     </View>
   );
 }
@@ -12,10 +19,27 @@ export default function Header() {
 const styles = StyleSheet.create({
   root: {
     height: 50,
-    backgroundColor: "green"
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    paddingHorizontal: 16
   },
   userMetaWrapper: {
     flex: 1,
-    backgroundColor: "dodgerblue"
+    backgroundColor: "dodgerblue",
+    flexDirection: "row"
+  },
+  buttonWrapper: {
+    flex: 0.1,
+    backgroundColor: "yellow"
+  },
+  avatarWrapper: {
+    flex: 0.15,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  avatarImg: {
+    backgroundColor: "green",
+    ...makeCircle(45)
   }
 });
