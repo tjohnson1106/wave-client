f307e07038a78543d7bea65fa80d3afcd27b53be
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-23 16:25:45 -0500

    cleaned up image area styling

--- src/components/ImageArea/ActionButtons.js
+++ src/components/ImageArea/ActionButtons.js
@@ -2,23 +2,25 @@ import React, { Component } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
+import { makeHitSlop } from "../../utils/themes";
+
 class ActionButtons extends Component {
   state = {};
   render() {
     return (
       <View style={styles.root}>
         <View style={styles.actionsWrapper}>
-          <TouchableOpacity style={styles.actionButton}>
-            <AntDesign name="hearto" size={30} color="white" />
+          <TouchableOpacity hitSlop={makeHitSlop(20)} style={styles.actionButton}>
+            <AntDesign name="hearto" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton}>
-            <AntDesign name="message1" size={30} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton}>
-            <AntDesign name="retweet" size={30} color="white" />
+          <TouchableOpacity hitSlop={makeHitSlop(20)} style={styles.actionButton}>
+            <AntDesign name="message1" size={30} />
           </TouchableOpacity>
         </View>
-        <View>{}</View>
+        <View style={styles.fakeView} />
+        <TouchableOpacity hitSlop={makeHitSlop(20)} style={styles.shareWrapper}>
+          <AntDesign name="retweet" size={35} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -27,18 +29,27 @@ class ActionButtons extends Component {
 const styles = StyleSheet.create({
   root: {
     height: 50,
-    backgroundColor: "red",
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    flexDirection: "row"
   },
   actionsWrapper: {
     flex: 1,
-    backgroundColor: "dodgerblue",
-    flexDirection: "row"
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
   actionButton: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  fakeView: {
+    flex: 1.6
+  },
+  shareWrapper: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "flex-end"
   }
 });
 
